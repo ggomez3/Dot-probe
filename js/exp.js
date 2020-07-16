71d057f891f67fd5599689c7fb0bb71ab9962dc3
ggomez3 <67597052+ggomez3@users.noreply.github.com>
2020-07-16 12:27:38 -0500

    Update exp.js

--- js/exp.js
+++ js/exp.js
@@ -151,7 +151,7 @@ var timeline = [];
 var trial_1 = {
     type: 'html-keyboard-response',
     stimulus: 'Please enter the passcode you created in the survey.',
-    choices: jsPsych.ALL_KEYS
+    choices: 'Digit_Array.js'
 }
 
 timeline.push(trial_1);
