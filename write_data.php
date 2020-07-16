9df711488cfd9616b96c04a54e7af028e10d8543
ggomez3 <67597052+ggomez3@users.noreply.github.com>
2020-07-16 17:45:27 -0500

    Update write_data.php

--- write_data.php
+++ write_data.php
@@ -1,8 +1,8 @@
-?php
+<?php
 $post_data = json_decode(file_get_contents('php://input'), true); 
 // the directory "data" must be writable by the server
 $name = "data/".$post_data['dotprobedata'].".csv"; 
 $data = $post_data['filedata'];
 // write the file to disk
 file_put_contents($name, $data);
-?
+?>
