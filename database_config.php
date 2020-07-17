00267ca66699b2d3fe887cb8ee12aa253d961683
ggomez3 <67597052+ggomez3@users.noreply.github.com>
2020-07-17 16:44:45 -0500

    Update database_config.php

--- database_config.php
+++ database_config.php
@@ -1,8 +1,8 @@
 <?php
-  $servername = "localhost";
+  $servername = "Local Instance 3306";
   $port = 3306;
-  $username = "username";
-  $password = "password";
+  $username = "mysql.infoschema";
+  $password = "Tru2Kn0W";
   $dbname = "database";
   $table = "tablename";
 ?>
